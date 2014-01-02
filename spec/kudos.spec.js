8515e644e3a4f592f38711a68a71f18c281174c8
= <juri.strumpflohner@gmail.com>
2014-01-02 16:26:07 +0100

    refactoring on the specs

--- spec/kudos.spec.js
+++ spec/kudos.spec.js
@@ -1,6 +1,8 @@
 describe('The Kudoable element', function(){
 
-    var clock;
+    var clock,
+        fixture,
+        kudoable;
 
     beforeEach(function(){
         //add a dom element to test upon
@@ -8,58 +10,54 @@ describe('The Kudoable element', function(){
                         .addClass('js-fixture');
         $('body').append($fixture);
 
-        this._fixture = $fixture;
+        fixture = $fixture;
+
+        //instantiate the kudoable element
+        kudoable = fixture.kudoable();
 
         //setup sinon fake timers
         clock = sinon.useFakeTimers();
     });
 
     afterEach(function(){
-        this._fixture.remove();
+        fixture.remove();
 
         clock.restore();
     });
 
-    it('should have a fixture available', function(){
-        expect(this._fixture).not.toBe(undefined);
-        expect($('.js-fixture').length).toBe(1);
-    });
+    describe('when touching', function(){
 
+        beforeEach(function(){
+            kudoable.trigger('touchstart');
+        });
 
-    it('should add the active class when applying the kudo', function(){
-        var kudoable = this._fixture.kudoable();
-        
-        // act
-        kudoable.trigger('touchstart');
+        it('should activate itself', function(){
+            
+            // assert
+            expect(fixture.hasClass('active')).toBe(true);
+        });
 
-        // assert
-        expect(this._fixture.hasClass('active')).toBe(true);
-    });
+        it('should complete after 700ms', function(){
+            // act & assert
+            clock.tick(699);
+            expect(fixture.hasClass('active')).toBe(true);  
 
-    it('should not have the active class after 700 ms', function(){
-        var kudoable = this._fixture.kudoable();
-        kudoable.trigger('touchstart');
+            clock.tick(1);
+            expect(fixture.hasClass('active')).toBe(false);  
+        });
 
-        // act & assert
-        clock.tick(699);
-        expect(this._fixture.hasClass('active')).toBe(true);  
-
-        clock.tick(1);
-        expect(this._fixture.hasClass('active')).toBe(false);  
-    });
+    }); 
 
     describe('when completing', function(){
 
-
         it('should add the complete class to the element it is bound to', function(){
-            var kudoable = this._fixture.kudoable();
-
+            
             // act
             kudoable.trigger('touchstart');
             clock.tick(700);
 
             //assert
-            expect(this._fixture.hasClass('complete')).toBe(true);
+            expect(fixture.hasClass('complete')).toBe(true);
         });
 
     });
