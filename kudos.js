c4b3d7e1cc36fc82bf63ff1f14461b3cd874b524
= <juri.strumpflohner@gmail.com>
2014-01-02 15:01:45 +0100

    adds the basics for the kudos..like CSS etc

---
+++ kudos.js
@@ -0,0 +1,15 @@
+(function($, undefined){
+
+    var Kudoable;
+
+    Kudoable = (function() {
+
+        function Kudoable(element){
+            this.element = element;
+        }
+
+        return Kudoable;
+
+    })();
+
+})(jQuery);
