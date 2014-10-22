8fef87eb8ac55648f710f7d029672899a5a57ac8
Juri <juri.strumpflohner@gmail.com>
2014-10-23 00:15:41 +0200

    adds rewrite of firebase storage mechanism

--- src/kudos.firebase.js
+++ src/kudos.firebase.js
@@ -5,6 +5,43 @@
         key = document.location.pathname.replace(/[\/-]/g,''),
         kudoStore = new Firebase(firebaseUrl);
 
+
+    var ref = new Firebase("https://firekudos.firebaseio.com");
+    ref.onAuth(function(authData) {
+        // if (authData && isNewUser) {
+        //     // save the user's profile into Firebase so we can
+        //     // list users, use them in security rules, and show profiles
+        //     ref.child('users').child(authData.uid).set(authData);
+        // }
+        if (authData) {
+            // user authenticated with Firebase
+            console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+
+            new Firebase("https://firekudos.firebaseio.com/users/" + authData.uid).once('value', function(snapshot){
+                var user = snapshot.val();
+                if(user !== null){
+                    console.log('user already known');
+                }else{
+                    ref.child('users').child(authData.uid).set(authData);
+                }
+            });
+        } else {
+            // user is logged out
+        }
+    });
+
+    var authData = ref.getAuth();
+    if(authData == null){
+        console.log('Authenticating user');
+        ref.authAnonymously(function(err, authData) {
+            if(authData){
+                console.log('Logged in', authData);
+            }else{
+                console.log('Error authenticating');
+            }
+        });
+    }
+
     // slightly hacky way of updating the count
     var updateKudoCount = function(count){
         $('.count .num').html(count);
@@ -16,26 +53,41 @@
     }
 
     //retrieve the current kudo count
-    $.getJSON(firebaseUrl + '/' + key + ".json", function(result){
-        if(result){
-            result.count = result.count || 0;
-            updateKudoCount(result.count);
+    new Firebase("https://firekudos.firebaseio.com/kudos/" + key).once('value', function(snap){
+        var article = snap.val();
+        if(article !== null){
+            var likeCount = 0;
+            for(var prop in article.likes){
+                likeCount++;
+            }
+            updateKudoCount(likeCount);
+        }else{
+            updateKudoCount(0);
         }
     });
 
     $(document).on('kudo.added', function(e, data){
-        kudoStore.child(key).set({ count: data.count });
+        kudoStore.child(key).child('likes').child(authData.uid).set({
+            count: 1
+        });
     });
 
     $(document).on('kudo.removed', function(e, data){
-       kudoStore.child(key).set({ count: data.count }); 
+       kudoStore.child(key).child('likes').child(authData.uid).remove();
     });
 
     // listening for updates
     var kudoEntry = new Firebase(firebaseUrl + '/' + key);
     kudoEntry.on('value', function(snapshot){
-        if(snapshot && snapshot.val()){
-            updateKudoCount(snapshot.val().count);
+        if(snapshot){
+            var article = snapshot.val();
+            var likeCount = 0;
+            if(article){
+                for(var prop in article.likes){
+                    likeCount++;
+                }
+            }
+            updateKudoCount(likeCount);
         }
     });
 
